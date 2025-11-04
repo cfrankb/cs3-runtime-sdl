aa6dd2cfb76cfd19ac964407ba0a6c80912eb2eb
Frank B. <cfrankb.2000@gmail.com>
2025-11-04 04:16:05 -0500

    removed tools from cs3-runtime

--- tools/chars/bin/export.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make && build/makebin && python bin/makesrc.py
-cp out/*.h /home/cfrankb/toolkit/gcc/cs3-runtime-sdl/src/
-cp out/*.cpp /home/cfrankb/toolkit/gcc/cs3-runtime-sdl/src/
--- tools/sheet/bin/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ls data/ > data/list.txt
-make && build/sheet $@
